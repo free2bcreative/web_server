df8eb2ac296c3c819121e4e32438d83f30533d19
Mike Free <free2bcreative@gmail.com>
2013-11-02 13:33:52 -0600

    Quick change to the default port number.  Changed from 3000 to 8080.

--- web.py
+++ web.py
@@ -15,7 +15,7 @@ class Main:
     def parse_arguments(self):
         ''' parse arguments, which include '-p' for port '''
         parser = argparse.ArgumentParser(prog='Echo Server', description='A simple echo server that handles one client at a time', add_help=True)
-        parser.add_argument('-p', '--port', type=int, action='store', help='port the server will bind to',default=3000)
+        parser.add_argument('-p', '--port', type=int, action='store', help='port the server will bind to',default=8080)
         parser.add_argument('-d', '--debug', action='store_true', help='turn on debugging messages',default=False)
         self.args = parser.parse_args()
 
